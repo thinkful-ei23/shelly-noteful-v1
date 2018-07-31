c4c20b12665e4d87bb45353392862571d9a2934e
Shelly Leung <pontificable@yahoo.com>
2018-07-31 13:50:02 -0700

    simDB

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const express = require('express');
 const { PORT } = require('./config');
 //const morgan = require('morgan');
 const data = require('./db/notes');
+const simDB = require('./db/simDB');
+const notes = simDB.initialize(data);
 const app = express();
 
 app.use(express.static('public'));
