f3203e364245c69ed7e34d16d13ac520d4fc5380
Shelly Leung <pontificable@yahoo.com>
2018-08-01 15:51:22 -0700

    api mounted

--- server.js
+++ server.js
@@ -6,18 +6,18 @@
 const express = require('express');
 //this is an object {variable through exports known as PORT} *locally
 const { PORT } = require('./config');
+const notesRouter = require('./router/notes.router.js');
 const morgan = require('morgan');
 //const notesRouter = require('./router/notes.router');
 const app = express();
-const router = require('./router/notes.router.js');
+
 
 app.use(express.static('public'));
 app.use(express.json());
 app.use(morgan('dev'));
 //from mentor session ==> app.use(morgan('combined'));
 
-app.use(router);
-//app.use('/api', notesRouter);
+app.use('/api', notesRouter);
 
 app.listen(PORT, function() {
 	console.info(`Server listening on ${this.address().port}`);
