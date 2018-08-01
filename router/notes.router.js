6100949a75533deb97ea9c05a8759a15e79c11be
Shelly Leung <pontificable@yahoo.com>
2018-08-01 13:26:26 -0700

    notes.router.js

---
+++ router/notes.router.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+
+const simDB = require('./db/simDB');
+const notes = simDB.initialize(data);
+
+router.put('/api/notes/:id', (req, res, next) => {
+	const id = req.params.id;
+
+	/***** Never trust users - validate input *****/
+	const updateObj = {};
+	const updateFields = ['title', 'content'];
+
+	updateFields.forEach(field => {
+		if (field in req.body) {
+			updateObj[field] = req.body[field];
+		}
+	});
+
+	notes.update(id, updateObj, (err, item) => {
+		if (err) {
+			return next(err);
+		}
+		if (item) {
+			res.json(item);
+		} else {
+			next();
+		}
+	});
+});
+
+router.get('/api/notes', (req, res, next) => {
+	const { searchTerm } = req.query;
+
+	notes.filter(searchTerm, (err, list) => {
+		if (err) {
+			return next(err); 
+		}
+		res.json(list); 
+	});
+});
+
+router.get('/api/notes/:id', (req, res) => {
+	const id = req.params.id;
+	notes.find(id, (err, item) => {
+		if (err) {
+			return next(err);
+		}
+		if (item) {
+			res.json(item);
+		} else {
+			next();
+		}
+	});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,64 +7,16 @@ const express = require('express');
 //this is an object {variable through exports known as PORT} *locally
 const { PORT } = require('./config');
 const morgan = require('morgan');
+
 const data = require('./db/notes');
-const simDB = require('./db/simDB');
-const notes = simDB.initialize(data);
+
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.Router('./router/notes.router.js'));
 app.use(express.json());
 app.use(morgan('combined'));
 
-app.put('/api/notes/:id', (req, res, next) => {
-	const id = req.params.id;
-
-	/***** Never trust users - validate input *****/
-	const updateObj = {};
-	const updateFields = ['title', 'content'];
-
-	updateFields.forEach(field => {
-		if (field in req.body) {
-			updateObj[field] = req.body[field];
-		}
-	});
-
-	notes.update(id, updateObj, (err, item) => {
-		if (err) {
-			return next(err);
-		}
-		if (item) {
-			res.json(item);
-		} else {
-			next();
-		}
-	});
-});
-
-app.get('/api/notes', (req, res, next) => {
-	const { searchTerm } = req.query;
-
-	notes.filter(searchTerm, (err, list) => {
-		if (err) {
-			return next(err); 
-		}
-		res.json(list); 
-	});
-});
-
-app.get('/api/notes/:id', (req, res) => {
-	const id = req.params.id;
-	notes.find(id, (err, item) => {
-		if (err) {
-			return next(err);
-		}
-		if (item) {
-			res.json(item);
-		} else {
-			next();
-		}
-	});
-});
 
 app.use(function (req, res, next) {
 	let err = new Error('Not Found');
