7cca8aa23a353e5bc69a31f432e927e45883a66d
Shelly Leung <pontificable@yahoo.com>
2018-08-01 17:34:23 -0700

    delete

--- router/notes.router.js
+++ router/notes.router.js
@@ -8,6 +8,39 @@ const data = require('../db/notes');
 const simDB = require('../db/simDB');
 const notes = simDB.initialize(data);
 
+router.post('/notes', (req,res,next) => {
+	const { title, content } = req.body;
+	
+	const newItem = { title, content };
+	if (!newItem.title) {
+		const err = new Error('Missing `title` in request body');
+		err.status = 400;
+		return next(err);
+	}
+
+	notes.create(newItem, (err,item) => {
+		if (err) {
+			return next(err);
+		}
+		if (item) {
+			res.location(`http://${req.headers.host}/notes/${item.id}`).status(201)
+		} else {
+			next();
+		}
+	});
+});
+
+router.delete('/notes/:id', (req, res, next) => {
+	const id = req.params.id;
+
+	notes.delete(id, (err) => {
+		if (err) {
+			return next(err);
+		}
+		res.sendStatus(204);
+	});
+});
+
 router.put('/notes/:id', (req, res, next) => {
 	const id = req.params.id;
 
